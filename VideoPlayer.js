0316569daafe5a940669bbb53cd84db602ed275b
nsaunders10 <nsaunderswps@gmail.com>
2020-12-11 13:34:38 -0500

    fix jittery image loading

---
+++ VideoPlayer.js
@@ -0,0 +1,8 @@
+function MobileScaling(){
+	//console.log("Is Mobile");
+	$("#sideVideo").css({display: "none"});
+	$(document.body).find(".center").eq(0).css({
+		width: "95%",
+		"background-color": "white",
+		});
+}
