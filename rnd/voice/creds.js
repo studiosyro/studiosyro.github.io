a863c0016b92ec699e3c3755ac7ddaab3e3b5c4e
nsaunders10 <nsaunderswps@gmail.com>
2021-02-19 14:37:46 -0800

    Update creds.js

--- rnd/voice/creds.js
+++ rnd/voice/creds.js
@@ -1,3 +1,3 @@
 var _appID = 'd8c4ce2ba0e0488eaab5f6a84f55c82a';
-var _token = '006d8c4ce2ba0e0488eaab5f6a84f55c82aIAB6/wsZg4DlF+dzdX/MB1Uu+2prnCRK67BbwHuLk32TIdchpy4AAAAAEABwjq6P+w4sYAEAAQD7Dixg';
+var _token = '006d8c4ce2ba0e0488eaab5f6a84f55c82aIABnk1Qc/h5yGKfWXYyxQDlrii/qzSrOed/eulO+FGeGtNchpy4AAAAAEABwjq6PEo8xYAEAAQARjzFg';
 var _channel = 'Test Room';
