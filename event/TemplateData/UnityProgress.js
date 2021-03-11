ca685ca35fea9f4300dd7cd07c70365a402ad29a
nsaunders10 <nsaunderswps@gmail.com>
2021-03-11 13:52:15 -0800

    Going live

--- event/TemplateData/UnityProgress.js
+++ event/TemplateData/UnityProgress.js
@@ -6,7 +6,7 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {    
+  if (false && !unityInstance.progress) {    
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
     unityInstance.progress.empty = document.createElement("div");
@@ -17,8 +17,10 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.progress.appendChild(unityInstance.progress.full);
     unityInstance.container.appendChild(unityInstance.progress);
   }
-  unityInstance.progress.full.style.width = (100 * progress) + "%";
-  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  //unityInstance.progress.full.style.display = "none";
+ // unityInstance.progress.empty.style.display = "none";
+  //unityInstance.progress.full.style.width = (100 * progress) + "%";
+  //unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
   if (progress == 1)
-    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+    unityInstance.logo.style.display = "none";
 }
