03e9d28f94de644b34059ea874327d493862c09f
nsaunders10 <nsaunderswps@gmail.com>
2021-04-06 12:01:27 -0700

    Photography tests

---
+++ demo/photography/creds.js
@@ -0,0 +1,4 @@
+var _appID = '2b077712baa54b21b5e4e17b6039d2e8';
+var _token = '006d8c4ce2ba0e0488eaab5f6a84f55c82aIABnk1Qc/h5yGKfWXYyxQDlrii/qzSrOed/eulO+FGeGtNchpy4AAAAAEABwjq6PEo8xYAEAAQARjzFg';
+var _channel = 'Test Room';
+var userId = '';
